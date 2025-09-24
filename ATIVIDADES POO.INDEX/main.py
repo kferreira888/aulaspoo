1b769913ab04fc6db425220868de36dbe8f75724
kferreira888 <kfsilva888@gmail.com>
2025-09-24 20:31:38 -0300

    Add files via upload

---
+++ ATIVIDADES POO.INDEX/main.py
@@ -0,0 +1,6 @@
+from notebook import Notebook
+
+Notebook.exibir_marca('ASUS')
+
+Notebook.informar_modelo('ULTIMA GERACAO')
+
---
+++ ATIVIDADES POO.INDEX/notebook.py
@@ -0,0 +1,10 @@
+class Notebook:
+    def exibir_marca (marca):
+        mensagem='informaçoes do notebook: ' + marca
+        print(mensagem)
+        
+
+    def informar_modelo (modelo):
+        dados ='dados do notebook:'+ modelo
+        print (dados)
+    
